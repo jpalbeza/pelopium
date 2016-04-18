f3a6e909a4dfb2f97aa3ba5ddbfb3a48c51e4390
John Paul Albeza <jpalbeza@Johns-MacBook-Pro.local>
2016-04-19 09:00:22 +1000

    added answers for forclojure

--- p1_to_100_clj/pelopium/src/pelopium/core.clj
+++ p1_to_100_clj/pelopium/src/pelopium/core.clj
@@ -1,13 +1,40 @@
 (ns pelopium.core)
 (require 'clojure.set)
 
-(defn p1-trigo []
+(defn p1-trigo-v1 []
   (->> (set (range 3 1000 3))
        (clojure.set/union (set (range 5 1000 5)))
        (into [])
        (reduce +)))
 
-(defn fibo [n]
-  (if (or (zero? n) (= 1 n))
-    1
-    (+ (fibo (- n 1)) (fibo (- n 2)))))
+
+(defn muls_of [& muls]
+  (fn [start]
+    (->> muls
+         (map #(- % (mod start %)))
+         (reduce min)
+         (+ start) )))
+
+
+(defn p1-trigo-v2 [end]
+  (let [muls_of_3_5 (muls_of 3 5)]
+    (->> (iterate muls_of_3_5 3)
+         (take-while (partial > end))
+         (reduce +))))
+
+
+(defn p2-v1 [accepted? limit]
+  (loop [prev 0 fib 1 result 0]
+    (if (>= fib limit)
+      result
+      (recur fib
+             (+ fib prev)
+             (if (accepted? fib)
+               (+ result fib)
+               result)))))
+
+(defn jp-iter [f x] (lazy-seq (cons x (jp-iter f (f x)))))
+
+(defn jp-interleave [coll1 coll2]  (apply assoc {} (interleave coll1 coll2)))
+
+(defn jp-juxt [& funcs] (fn [& args] (vec (map #(apply % args) funcs))))
--- p1_to_100_clj/pelopium/test/pelopium/core_test.clj
+++ p1_to_100_clj/pelopium/test/pelopium/core_test.clj
@@ -3,7 +3,34 @@
             [pelopium.core :refer :all]))
 
 (deftest p1-trigo-test
-  (testing "p1-trigo"
-    (is (= (p1-trigo) 233168))))
+  (testing "p1-trigo-v1"
+    (is (= (p1-trigo-v1) 233168))))
 
+(deftest p1-trigo-v2-test
+  (testing "p1-trigo-v2"
+    (is (= (p1-trigo-v2 1000) 233168))))
 
+(deftest p1-v1
+  (testing "p2-v1"
+    (is (= (p2-v1 (partial even?) 4000000)) 4613732)))
+
+(deftest jp-iter-test
+  (testing "jp-iter"
+    (is (and
+          (= (take 5 (jp-iter #(* 2 %) 1)) [1 2 4 8 16])
+          (= (take 100 (jp-iter inc 0)) (take 100 (range)))
+          (= (take 9 (jp-iter #(inc (mod % 3)) 1)) (take 9 (cycle [1 2 3])))))))
+
+(deftest jp-interleave-test
+  (testing "jp-interleave"
+    (is (and
+          (= (jp-interleave [:a :b :c] [1 2 3]) {:a 1, :b 2, :c 3})
+          (= (jp-interleave [1 2 3 4] ["one" "two" "three"]) {1 "one", 2 "two", 3 "three"})
+          (= (jp-interleave [:foo :bar] ["foo" "bar" "baz"]) {:foo "foo", :bar "bar"})))))
+
+(deftest jp-juxt-test
+  (testing "jp-juxt"
+    (is (and
+          (= [21 6 1] ((jp-juxt + max min) 2 3 5 1 6 4))
+          (= ["HELLO" 5] ((jp-juxt #(.toUpperCase %) count) "hello"))
+          (= [2 6 4] ((jp-juxt :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))))))
